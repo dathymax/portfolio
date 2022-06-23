9bad5cefd889e6ba8891296d5edf4596e1e9b12d
Do Tuan Dat <dathymax@gmail.com>
2022-06-23 21:48:00 +0700

    update

--- src/data.js
+++ src/data.js
@@ -1,268 +1,296 @@
 //  icons
 import {
-  FiYoutube,
-  FiInstagram,
-  FiGithub,
-  FiDribbble,
-  FiLayout,
-  FiSettings,
-  FiPenTool,
-  FiTag,
-  FiMail,
-  FiMapPin,
-} from 'react-icons/fi';
+	FiYoutube,
+	FiInstagram,
+	FiGithub,
+	FiDribbble,
+	FiLayout,
+	FiSettings,
+	FiPenTool,
+	FiTag,
+	FiMail,
+	FiMapPin,
+} from "react-icons/fi";
 
 // companies icons
-import FreelancerBrandIcon from './assets/img/brands/freelancer.png';
-import UpworkBrandIcon from './assets/img/brands/upwork.png';
-import FiverBrandIcon from './assets/img/brands/fiverr.png';
-import BehanceBrandIcon from './assets/img/brands/behance.png';
-import DribbbleBrandIcon from './assets/img/brands/dribbble.png';
+import FreelancerBrandIcon from "./assets/img/brands/freelancer.png";
+import UpworkBrandIcon from "./assets/img/brands/upwork.png";
+import FiverBrandIcon from "./assets/img/brands/fiverr.png";
+import BehanceBrandIcon from "./assets/img/brands/behance.png";
+import DribbbleBrandIcon from "./assets/img/brands/dribbble.png";
 
 // projects images
-import Project1 from './assets/img/projects/p1.webp';
-import Project2 from './assets/img/projects/p2.webp';
-import Project3 from './assets/img/projects/p3.webp';
-import Project4 from './assets/img/projects/p4.webp';
-import Project5 from './assets/img/projects/p5.webp';
-import Project6 from './assets/img/projects/p6.webp';
+import TravelProject from "./images/travel.PNG";
+import ManageEmployees from "./images/manage-employees.PNG";
+import MyCVProject from "./images/mycv-app.PNG";
+import TheBandProject from "./images/theband-app.PNG";
+import PortfolioProject from "./images/portfolio-html.PNG";
+import LoveTravel from "./images/love-travel.PNG";
+import LoveTravelSearch1 from "./images/love-travel-search1.PNG";
+import LoveTravelSearch2 from "./images/love-travel-search2.PNG";
+import LoveTravelSport from "./images/love-travel-sport.PNG";
+import AirBnb from "./images/airbnb.PNG";
 
 // skills images
-import SkillImg1 from './assets/img/skills/html5.png';
-import SkillImg2 from './assets/img/skills/css3.png';
-import SkillImg3 from './assets/img/skills/js.png';
-import SkillImg4 from './assets/img/skills/reactjs.png';
-import SkillImg5 from './assets/img/skills/nextjs.png';
-import SkillImg6 from './assets/img/skills/nodejs.png';
-import SkillImg7 from './assets/img/skills/git.png';
-import SkillImg8 from './assets/img/skills/figma.png';
+import SkillImg1 from "./assets/img/skills/html5.png";
+import SkillImg2 from "./assets/img/skills/css3.png";
+import SkillImg3 from "./assets/img/skills/js.png";
+import SkillImg4 from "./assets/img/skills/reactjs.png";
+import SkillImg5 from "./assets/img/skills/nextjs.png";
+import SkillImg6 from "./assets/img/skills/nodejs.png";
+import SkillImg7 from "./assets/img/skills/git.png";
+import SkillImg8 from "./assets/img/skills/figma.png";
 
 // testimonial images
-import TestiImage1 from './assets/img/testimonials/testimonial-1.webp';
-import TestiImage2 from './assets/img/testimonials/testimonial-2.webp';
-import TestiImage3 from './assets/img/testimonials/testimonial-3.webp';
+import TestiImage1 from "./assets/img/testimonials/testimonial-1.webp";
+import TestiImage2 from "./assets/img/testimonials/testimonial-2.webp";
+import TestiImage3 from "./assets/img/testimonials/testimonial-3.webp";
 
 // navigation
 export const navigation = [
-  {
-    name: 'home',
-    href: 'home',
-  },
-  {
-    name: 'about',
-    href: 'about',
-  },
-  {
-    name: 'portfolio',
-    href: 'portfolio',
-  },
-  {
-    name: 'services',
-    href: 'services',
-  },
-  {
-    name: 'testimonials',
-    href: 'testimonials',
-  },
-  {
-    name: 'contact',
-    href: 'contact',
-  },
+	{
+		name: "home",
+		href: "home",
+	},
+	{
+		name: "about",
+		href: "about",
+	},
+	{
+		name: "portfolio",
+		href: "portfolio",
+	},
+	{
+		name: "services",
+		href: "services",
+	},
+	{
+		name: "testimonials",
+		href: "testimonials",
+	},
+	{
+		name: "contact",
+		href: "contact",
+	},
 ];
 
 // social
 export const social = [
-  {
-    icon: <FiYoutube />,
-    href: '',
-  },
-  {
-    icon: <FiInstagram />,
-    href: '',
-  },
-  {
-    icon: <FiGithub />,
-    href: '',
-  },
-  {
-    icon: <FiDribbble />,
-    href: '',
-  },
+	{
+		icon: <FiYoutube />,
+		href: "",
+	},
+	{
+		icon: <FiInstagram />,
+		href: "",
+	},
+	{
+		icon: <FiGithub />,
+		href: "",
+	},
+	{
+		icon: <FiDribbble />,
+		href: "",
+	},
 ];
 
 // companies
 export const brands = [
-  {
-    img: FreelancerBrandIcon,
-    href: '',
-  },
-  {
-    img: UpworkBrandIcon,
-    href: '',
-  },
-  {
-    img: FiverBrandIcon,
-    href: '',
-  },
-  {
-    img: BehanceBrandIcon,
-    href: '',
-  },
-  {
-    img: DribbbleBrandIcon,
-    href: '',
-  },
+	{
+		img: FreelancerBrandIcon,
+		href: "",
+	},
+	{
+		img: UpworkBrandIcon,
+		href: "",
+	},
+	{
+		img: FiverBrandIcon,
+		href: "",
+	},
+	{
+		img: BehanceBrandIcon,
+		href: "",
+	},
+	{
+		img: DribbbleBrandIcon,
+		href: "",
+	},
 ];
 
 // projects
 export const projectsData = [
-  {
-    id: '1',
-    image: Project1,
-    name: 'project name 1',
-    category: 'UI/UX design',
-  },
-  {
-    id: '2',
-    image: Project2,
-    name: 'project name 2',
-    category: 'web development',
-  },
-  {
-    id: '3',
-    image: Project3,
-    name: 'project name 3',
-    category: 'UI/UX design',
-  },
-  {
-    id: '4',
-    image: Project4,
-    name: 'project name 4',
-    category: 'branding',
-  },
-  {
-    id: '5',
-    image: Project5,
-    name: 'project name 5',
-    category: 'web development',
-  },
-  {
-    id: '6',
-    image: Project6,
-    name: 'project name 6',
-    category: 'web development',
-  },
+	{
+		id: "1",
+		image: TravelProject,
+		name: "Travel",
+		category: "ReactJs",
+	},
+	{
+		id: "2",
+		image: MyCVProject,
+		name: "My CV app",
+		category: "ReactJs",
+	},
+	{
+		id: "3",
+		image: TheBandProject,
+		name: "The band",
+		category: "ReactJs",
+	},
+	{
+		id: "4",
+		image: PortfolioProject,
+		name: "Portfolio HTML/CSS/JS",
+		category: "HTML/CSS/JS",
+	},
+	{
+		id: "5",
+		image: LoveTravel,
+		name: "Love travel",
+		category: "HTML/CSS/JS",
+	},
+	{
+		id: "6",
+		image: LoveTravelSearch1,
+		name: "Love travel search 1",
+		category: "HTML/CSS/JS",
+	},
+	{
+		id: "7",
+		image: LoveTravelSearch2,
+		name: "Love travel search 2",
+		category: "HTML/CSS/JS",
+	},
+	{
+		id: "8",
+		image: LoveTravelSport,
+		name: "Love travel sport",
+		category: "HTML/CSS/JS",
+	},
+	{
+		id: "9",
+		image: AirBnb,
+		name: "AirBnb",
+		category: "HTML/CSS/JS",
+	},
+	{
+		id: "10",
+		image: ManageEmployees,
+		name: "Manage employees",
+		category: "ReactJs",
+	},
 ];
 
 // projects
 export const projectsNav = [
-  {
-    name: 'all',
-  },
-  {
-    name: 'UI/UX Design',
-  },
-  {
-    name: 'web development',
-  },
-  {
-    name: 'branding',
-  },
+	{
+		name: "All",
+	},
+	{
+		name: "HTML/CSS/JS",
+	},
+	{
+		name: "ReactJs",
+	},
+	{
+		name: "NodeJs",
+	},
 ];
 
 // skill
 export const skills = [
-  {
-    image: SkillImg1,
-  },
-  {
-    image: SkillImg2,
-  },
-  {
-    image: SkillImg3,
-  },
-  {
-    image: SkillImg4,
-  },
-  {
-    image: SkillImg5,
-  },
-  {
-    image: SkillImg6,
-  },
-  {
-    image: SkillImg7,
-  },
-  {
-    image: SkillImg8,
-  },
+	{
+		image: SkillImg1,
+	},
+	{
+		image: SkillImg2,
+	},
+	{
+		image: SkillImg3,
+	},
+	{
+		image: SkillImg4,
+	},
+	{
+		image: SkillImg5,
+	},
+	{
+		image: SkillImg6,
+	},
+	{
+		image: SkillImg7,
+	},
+	{
+		image: SkillImg8,
+	},
 ];
 
 // services
 export const services = [
-  {
-    icon: <FiLayout />,
-    name: 'Web Design',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio perferendis volm quibusdam ullam qui dolore.',
-  },
-  {
-    icon: <FiSettings />,
-    name: 'Web Development',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio perferendis volm quibusdam ullam qui dolore.',
-  },
-  {
-    icon: <FiPenTool />,
-    name: 'Branding',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio perferendis volm quibusdam ullam qui dolore.',
-  },
-  {
-    icon: <FiTag />,
-    name: 'SEO',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio perferendis volm quibusdam ullam qui dolore.',
-  },
+	{
+		icon: <FiLayout />,
+		name: "Web Design",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio perferendis volm quibusdam ullam qui dolore.",
+	},
+	{
+		icon: <FiSettings />,
+		name: "Web Development",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio perferendis volm quibusdam ullam qui dolore.",
+	},
+	{
+		icon: <FiPenTool />,
+		name: "Branding",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio perferendis volm quibusdam ullam qui dolore.",
+	},
+	{
+		icon: <FiTag />,
+		name: "SEO",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio perferendis volm quibusdam ullam qui dolore.",
+	},
 ];
 
 // testimonials
 export const testimonials = [
-  {
-    authorImg: TestiImage1,
-    authorText:
-      'Love the simplicity of the service and the prompt customer support. We can’t imagine working without it.',
-    authorName: 'Olivia Doe',
-    authorPosition: 'Head of Design, Google',
-  },
-  {
-    authorImg: TestiImage2,
-    authorText:
-      'Love the simplicity of the service and the prompt customer support. We can’t imagine working without it.',
-    authorName: 'Olivia Doe',
-    authorPosition: 'Head of Design, Google',
-  },
-  {
-    authorImg: TestiImage3,
-    authorText:
-      'Love the simplicity of the service and the prompt customer support. We can’t imagine working without it.',
-    authorName: 'Olivia Doe',
-    authorPosition: 'Head of Design, Google',
-  },
+	{
+		authorImg: TestiImage1,
+		authorText:
+			"Love the simplicity of the service and the prompt customer support. We can’t imagine working without it.",
+		authorName: "Olivia Doe",
+		authorPosition: "Head of Design, Google",
+	},
+	{
+		authorImg: TestiImage2,
+		authorText:
+			"Love the simplicity of the service and the prompt customer support. We can’t imagine working without it.",
+		authorName: "Olivia Doe",
+		authorPosition: "Head of Design, Google",
+	},
+	{
+		authorImg: TestiImage3,
+		authorText:
+			"Love the simplicity of the service and the prompt customer support. We can’t imagine working without it.",
+		authorName: "Olivia Doe",
+		authorPosition: "Head of Design, Google",
+	},
 ];
 
 // contact
 export const contact = [
-  {
-    icon: <FiMail />,
-    title: 'Have a question?',
-    subtitle: 'I am here to help you.',
-    description: 'Email me at hello@youremail.com',
-  },
-  {
-    icon: <FiMapPin />,
-    title: 'Current Location',
-    subtitle: 'Bucharest, Romania',
-    description: 'Serving clients worldwide',
-  },
+	{
+		icon: <FiMail />,
+		title: "Have a question?",
+		subtitle: "I am here to help you.",
+		description: "Email me at hello@youremail.com",
+	},
+	{
+		icon: <FiMapPin />,
+		title: "Current Location",
+		subtitle: "Bucharest, Romania",
+		description: "Serving clients worldwide",
+	},
 ];
